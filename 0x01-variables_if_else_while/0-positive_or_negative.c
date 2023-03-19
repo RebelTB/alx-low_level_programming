88b3f98b2d53d5212c25109aa15e7f7a52504f03
RebelTB <toheebbello09@gmail.com>
2023-03-19 16:53:03 -0700

    if/else_commands

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,13 @@
 */
 int main (void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand () - RAND_MAX/2;
-        if (n > 0)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -21,7 +21,7 @@ int main (void)
 	}
 	else
 	{
-		printf("%d is negative\n", n);	
+		printf("%d is negative\n", n);
 	}
 	return (0);
-} 
+}
